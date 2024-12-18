2ee4e111937205f706e7ad0264b331f4d7e769f2
Joshua Trịnh <103560993+JoshuaTrinh5262@users.noreply.github.com>
2024-12-18 14:59:15 +0700

    Update include.js

--- include.js
+++ include.js
@@ -17,9 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.querySelector(".menu-toggle");
   const navLinks = document.querySelector(".nav-links");
 
-  if (menuToggle && navLinks) {
-    menuToggle.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
-  }
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 });
